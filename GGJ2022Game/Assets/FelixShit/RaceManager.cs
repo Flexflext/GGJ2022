80e94d0576c695266377bff2b8516e898e9a2720
Flexflext <71442255+Flexflext@users.noreply.github.com>
2022-01-22 14:38:03 +0100

    vdfb ohjnuisvdbhjnuikoascdvnbjuiodsvnjiklo

--- GGJ2022Game/Assets/FelixShit/RaceManager.cs
+++ GGJ2022Game/Assets/FelixShit/RaceManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RaceManager : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class RaceManager : MonoBehaviour
     [SerializeField] private Transform firstPlayer;
     [SerializeField] private Transform secondPlayer;
     [SerializeField] private CameraControl camControl;
+
+    [SerializeField] private TMP_Text firstPlayerScore;
+    [SerializeField] private TMP_Text secondPlayerScore;
+    [SerializeField] private TMP_Text infoText;
+    [SerializeField] private TMP_Text spaceText;
+    [SerializeField] private TMP_Text timerText;
 
     private bool firstIsInFront = true;
 
@@ -24,9 +31,35 @@ public class RaceManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        string first = "";
+        string second = "";
+
+        GameManager.Instance.ChangeScore(ref first, ref second);
+
+        firstPlayerScore.text = first;
+        secondPlayerScore.text = second;
+
+        Time.timeScale = 0;
+        StartCoroutine(C_StartWait());
+    }
+
     private void Update()
     {
         SetFirstAndLastPlayer();
+    }
+
+    public void SetPlayerScoreText(int _first, int _second)
+    {
+        firstPlayerScore.text = _first.ToString();
+        secondPlayerScore.text = _second.ToString();
+    }
+
+    public void SetTexts(string _tosay)
+    {
+        infoText.text = _tosay;
+        spaceText.gameObject.SetActive(true);
     }
 
     private void SetFirstAndLastPlayer()
@@ -43,6 +76,26 @@ public class RaceManager : MonoBehaviour
         }
     }
 
+    private IEnumerator C_StartWait()
+    {
 
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == 3)
+            {
+                timerText.text = "GO";
+                Time.timeScale = 1;
+            }
+            else
+            {
+                timerText.text = (3 - i).ToString();
+            }
+
+            yield return new WaitForSecondsRealtime(1);
+        }
+
+        
+        timerText.text = "";
+    }
 
 }
